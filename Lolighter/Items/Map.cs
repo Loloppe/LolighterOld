7bc897bf67d0a61c8dffc29a769b338530cb28b9
Loloppe <loloppe9999@hotmail.com>
2021-05-20 12:40:04 -0400

    Updated file structure

--- Lolighter/Items/Map.cs
+++ Lolighter/Items/Map.cs
@@ -1,68 +1,74 @@
 ﻿namespace Lolighter.Items
 {
-        public class Rootobject
+    public class Rootobject
+    {
+        public string _version { get; set; }
+        public _Customdata _customData { get; set; }
+        public _Events[] _events { get; set; }
+        public _Notes[] _notes { get; set; }
+        public _Obstacles[] _obstacles { get; set; }
+        public object[] _waypoints { get; set; }
+    }
+
+    public class _Customdata
+    {
+        public double _time { get; set; }
+        public object[] _BPMChanges { get; set; }
+        public _Bookmarks[] _bookmarks { get; set; }
+    }
+
+    public class _Bookmarks
+    {
+        public double _time { get; set; }
+        public string _name { get; set; }
+    }
+
+    public class _Bpmchanges
+    {
+        public double _BPM { get; set; }
+        public double _time { get; set; }
+        public double _beatsPerBar { get; set; }
+        public double _metronomeOffset { get; set; }
+    }
+
+    public class _Events
+    {
+        public _Events(double v1, int v2, int v3)
         {
-            public string _version { get; set; }
-            public _Events[] _events { get; set; }
-            public _Notes[] _notes { get; set; }
-            public _Obstacles[] _obstacles { get; set; }
-            public _Customdata _customData { get; set; }
+            _time = v1;
+            _type = v2;
+            _value = v3;
         }
 
-        public class _Customdata
+        public double _time { get; set; }
+        public int _type { get; set; }
+        public int _value { get; set; }
+    }
+
+    public class _Notes
+    {
+        public _Notes(double v1, int v2, int v3, int type, int v4)
         {
-            public double _time { get; set; }
-            public _Bpmchanges[] _BPMChanges { get; set; }
-            public object[] _bookmarks { get; set; }
+            _time = v1;
+            _lineIndex = v2;
+            _lineLayer = v3;
+            _type = type;
+            _cutDirection = v4;
         }
 
-        public class _Bpmchanges
-        {
-            public double _BPM { get; set; }
-            public double _time { get; set; }
-            public double _beatsPerBar { get; set; }
-            public double _metronomeOffset { get; set; }
-        }
+        public double _time { get; set; }
+        public int _lineIndex { get; set; }
+        public int _lineLayer { get; set; }
+        public int _type { get; set; }
+        public int _cutDirection { get; set; }
+    }
 
-        public class _Events
-        {
-            public _Events(double v1, int v2, int v3)
-            {
-                _time = v1;
-                _type = v2;
-                _value = v3;
-            }
-
-            public double _time { get; set; }
-            public int _type { get; set; }
-            public int _value { get; set; }
-        }
-
-        public class _Notes
-        {
-            public _Notes(double v1, int v2, int v3, int type, int v4)
-            {
-                _time = v1;
-                _lineIndex = v2;
-                _lineLayer = v3;
-                _type = type;
-                _cutDirection = v4;
-            }
-
-            public double _time { get; set; }
-            public int _lineIndex { get; set; }
-            public int _lineLayer { get; set; }
-            public int _type { get; set; }
-            public int _cutDirection { get; set; }
-        }
-
-        public class _Obstacles
-        {
-            public double _time { get; set; }
-            public int _lineIndex { get; set; }
-            public int _type { get; set; }
-            public double _duration { get; set; }
-            public int _width { get; set; }
-        }
+    public class _Obstacles
+    {
+        public double _time { get; set; }
+        public int _lineIndex { get; set; }
+        public int _type { get; set; }
+        public double _duration { get; set; }
+        public int _width { get; set; }
+    }
 }
-
